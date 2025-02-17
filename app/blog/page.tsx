b6c1f05c40805814c723114e5760ee9cf4de292e
Colm McSharry <colm.mcsharry@bonkers.ie>
2025-02-17 22:39:42 +0000

    test

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,11 @@
-import { title } from "@/components/primitives";
-
 export default function BlogPage() {
   return (
     <div>
-      <h1 className={title()}>Blog</h1>
+      <h1 className="text-5xl mb-8 font-bold">Blog</h1>
+      <p className="text-lg mb-8">
+        Some of the latest blog posts about the dating scene in Ireland.
+      </p>
+      <p>This will feature guest posts from an array of people</p>
     </div>
   );
 }
